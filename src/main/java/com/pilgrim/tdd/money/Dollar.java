8866bf6944e35d5d9daa5898290ea1428b8220a2
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2021-11-10 23:38:39 +0900

    P315 重複をtimesに移動 green

--- src/main/java/com/pilgrim/tdd/money/Dollar.java
+++ src/main/java/com/pilgrim/tdd/money/Dollar.java
@@ -2,13 +2,13 @@ package com.pilgrim.tdd.money;
 
 public class Dollar {
 
-  int amount = 5 * 2;
+  int amount;
 
   public Dollar(int amount) {
   }
 
   // multiplier・・・乗数
   void times(int multiplier){
-
+    amount = 5 * 2;
   }
 }
