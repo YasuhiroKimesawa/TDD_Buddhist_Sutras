7e57e59fdc80c154af1ea322d9979b865ccb9394
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2021-11-10 23:40:53 +0900

    P336 5の重複を消した

--- src/main/java/com/pilgrim/tdd/money/Dollar.java
+++ src/main/java/com/pilgrim/tdd/money/Dollar.java
@@ -5,10 +5,11 @@ public class Dollar {
   int amount;
 
   public Dollar(int amount) {
+    this.amount = amount;
   }
 
   // multiplier・・・乗数
   void times(int multiplier){
-    amount = 5 * 2;
+    amount = amount * 2;
   }
 }
