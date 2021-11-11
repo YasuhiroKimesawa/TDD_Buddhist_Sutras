dddd35671720ab10ea19d5ef8513e6a9b8b2a159
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2021-11-11 23:29:41 +0900

    P1055 variable currency

--- src/main/java/com/pilgrim/tdd/money/Dollar.java
+++ src/main/java/com/pilgrim/tdd/money/Dollar.java
@@ -2,8 +2,11 @@ package com.pilgrim.tdd.money;
 
 public class Dollar extends Money{
 
+  private String currency;
+
   public Dollar(int amount) {
     this.amount = amount;
+    currency = "USD";
   }
 
   // multiplier・・・乗数
@@ -13,6 +16,6 @@ public class Dollar extends Money{
 
   @Override
   String currency() {
-    return "USD";
+    return currency;
   }
 }
--- src/main/java/com/pilgrim/tdd/money/Franc.java
+++ src/main/java/com/pilgrim/tdd/money/Franc.java
@@ -2,8 +2,11 @@ package com.pilgrim.tdd.money;
 
 public class Franc extends Money{
 
+  private String currency;
+
   public Franc(int amount) {
     this.amount = amount;
+    currency = "CHF";
   }
 
   // multiplier・・・乗数
@@ -13,6 +16,6 @@ public class Franc extends Money{
 
   @Override
   String currency() {
-    return "CHF";
+    return currency;
   }
 }
