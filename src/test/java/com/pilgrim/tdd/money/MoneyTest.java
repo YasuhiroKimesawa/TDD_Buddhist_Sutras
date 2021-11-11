4371dba36db4a6ef74aea673c53a6705df591c1e
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2021-11-11 23:11:19 +0900

    P909 red modify Money factory

--- src/test/java/com/pilgrim/tdd/money/MoneyTest.java
+++ src/test/java/com/pilgrim/tdd/money/MoneyTest.java
@@ -7,7 +7,7 @@ public class MoneyTest {
   // Multiplication・・・乗算
   @Test
   public void testMultiplication(){
-    Dollar five = new Dollar(5);
+    Dollar five = Money.dollar(5);
     Assertions.assertEquals(new Dollar(10), five.times(2));
 
     Assertions.assertEquals(new Dollar(15), five.times(3));
