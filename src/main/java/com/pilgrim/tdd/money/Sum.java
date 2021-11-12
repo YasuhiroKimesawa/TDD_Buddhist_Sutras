2f1ed7a56fbdad9105d7536656bea775e49c961e
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2021-11-12 22:20:11 +0900

    P1629 green Sum constructor

--- src/main/java/com/pilgrim/tdd/money/Sum.java
+++ src/main/java/com/pilgrim/tdd/money/Sum.java
@@ -1,6 +1,11 @@
 package com.pilgrim.tdd.money;
 
-public class Sum {
+public class Sum implements Expression{
   Money augend;
   Money addend;
+
+  public Sum(Money augend, Money addend) {
+    this.augend = augend;
+    this.addend = addend;
+  }
 }
