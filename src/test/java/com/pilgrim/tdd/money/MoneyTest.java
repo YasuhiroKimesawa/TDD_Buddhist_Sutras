ab6feefe6c53f1dbdc553a9d19815de38a1e5ab1
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2021-11-12 22:40:51 +0900

    P1784 add test

--- src/test/java/com/pilgrim/tdd/money/MoneyTest.java
+++ src/test/java/com/pilgrim/tdd/money/MoneyTest.java
@@ -63,4 +63,14 @@ public class MoneyTest {
     Money result = bank.reduce(Money.dollar(1), "USD");
     Assertions.assertEquals(Money.dollar(1), result);
   }
+
+  @Test
+  public void testReduceMoneyDifferentCurrency(){
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+
+    Money result = bank.reduce(Money.franc(2), "USD");
+
+    Assertions.assertEquals(Money.dollar(1), result);
+  }
 }
