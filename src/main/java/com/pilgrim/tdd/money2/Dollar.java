b633dc8af2bee4c6bbd72cdb1c7e00eedac34a64
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2022-04-20 17:17:05 +0900

    1053 GREEN currency val

--- src/main/java/com/pilgrim/tdd/money2/Dollar.java
+++ src/main/java/com/pilgrim/tdd/money2/Dollar.java
@@ -2,12 +2,20 @@ package com.pilgrim.tdd.money2;
 
 class Dollar extends Money{
 
+  private String currency;
+
   Dollar(int amount) {
     super(amount);
+    this.currency = "USD";
   }
 
   Money times(int multiplier){
     return new Dollar(amount * multiplier);
+  }
+
+  @Override
+  String currency() {
+    return currency;
   }
 
 }
--- src/main/java/com/pilgrim/tdd/money2/Franc.java
+++ src/main/java/com/pilgrim/tdd/money2/Franc.java
@@ -2,12 +2,20 @@ package com.pilgrim.tdd.money2;
 
 public class Franc extends Money{
 
+  private String currency;
+
   Franc(int amount) {
     super(amount);
     this.amount = amount;
+    currency = "CHF";
   }
 
   Money times(int multiplier){
     return new Franc(amount * multiplier);
+  }
+
+  @Override
+  String currency() {
+    return currency;
   }
 }
