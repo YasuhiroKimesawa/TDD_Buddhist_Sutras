90da48613e5394a093ba99465d15dc3f268b9880
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2022-06-29 17:23:32 +0900

    1451 GREEN  足し算実装

--- src/test/java/com/pilgrim/tdd/money2/MoneyTest.java
+++ src/test/java/com/pilgrim/tdd/money2/MoneyTest.java
@@ -33,7 +33,7 @@ public class MoneyTest {
   @Test
   public void testSimpleAddition(){
     Money sum = Money.dollar(5).plus(Money.dollar(5));
-    assertEquals(Money.dollar(5), sum);
+    assertEquals(Money.dollar(10), sum);
   }
 
 }
